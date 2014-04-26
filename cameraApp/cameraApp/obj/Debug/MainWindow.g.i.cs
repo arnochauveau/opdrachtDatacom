691184daeaedaa7c4e32531991f5d509da85081f
arnoch <arnochauveau@gmail.com>
2014-04-26 10:37:26 +0200

    gui

--- cameraApp/cameraApp/obj/Debug/MainWindow.g.i.cs
+++ cameraApp/cameraApp/obj/Debug/MainWindow.g.i.cs
@@ -67,6 +67,17 @@ namespace cameraApp {
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
         void System.Windows.Markup.IComponentConnector.Connect(int connectionId, object target) {
+            switch (connectionId)
+            {
+            case 1:
+            
+            #line 4 "..\..\MainWindow.xaml"
+            ((cameraApp.MainWindow)(target)).Loaded += new System.Windows.RoutedEventHandler(this.Window_Loaded);
+            
+            #line default
+            #line hidden
+            return;
+            }
             this._contentLoaded = true;
         }
     }
