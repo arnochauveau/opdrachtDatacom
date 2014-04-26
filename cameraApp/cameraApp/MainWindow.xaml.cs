1a1072f26ce2c9814827e885d273d55f890ac397
arnoch <arnochauveau@gmail.com>
2014-04-26 13:38:03 +0200

    updaten van sliders

--- cameraApp/cameraApp/MainWindow.xaml.cs
+++ cameraApp/cameraApp/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace cameraApp
     public partial class MainWindow : Window
     {
         private DispatcherTimer tmr = new DispatcherTimer();
+        private Double[] arrpos = new Double[4];
         public MainWindow()
         {
             InitializeComponent();
@@ -38,7 +39,7 @@ namespace cameraApp
 
         private void initTimer()
         {
-            tmr.Interval = TimeSpan.FromMilliseconds(1000);
+            tmr.Interval = TimeSpan.FromMilliseconds(5);
             tmr.Tick += tmr_Tick;
             tmr.Start();
         }
@@ -64,6 +65,30 @@ namespace cameraApp
             im.EndInit();
 
             img.Source = im;
+
+            HttpWebRequest req2 = (HttpWebRequest)HttpWebRequest.Create("http://172.23.49.1/axis-cgi/com/ptz.cgi?query=position");
+
+            req2.Credentials = new NetworkCredential("student", "nmct");
+
+            req2.GetResponse();
+            HttpWebResponse resp2 = (HttpWebResponse)req2.GetResponse();
+            Stream r = resp2.GetResponseStream();
+            StreamReader readStream = new StreamReader(r);
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                arrpos[i] = Convert.ToDouble( readStream.ReadLine().Split('=')[1]);
+            }
+            pan.Value = arrpos[0];
+            tilt.Value = arrpos[1];
+            zoom1.Value = arrpos[2];
+            focus.Value = arrpos[3];
+            
+            readStream.Close();
+            r.Close();
+            req2.Abort();
+            resp2.Dispose();
 
         }
 
