c42f7582e425e29f80d9fa6810e55a59ca2c2a17
arnoch <arnochauveau@gmail.com>
2014-04-28 10:48:44 +0200

    scherm werkt yesyesyes

--- sketch_arduino/sketch_arduino.ino
+++ sketch_arduino/sketch_arduino.ino
@@ -9,13 +9,42 @@
 // d6 = 5          // 13
 // d7 = 4          // 14
 
+#include <LiquidCrystal.h>
 int buttonpin = 12; //kies de pin voor de BUTTON
 int val = 0; //variable voor status buttonpin
 
+int lcdRS = 10;
+int lcdRW = 9;
+
+int lcdEN = 8;
+
+int lcdd4 =7;
+int lcdd5 = 6;
+int lcdd6 = 5;
+int lcdd7 = 4;
+
+LiquidCrystal lcd(lcdRS,lcdRW,lcdEN,lcdd4,lcdd5,lcdd6,lcdd7);
+
 void setup()
 {
+  lcd.begin(16,2);
   pinMode(buttonpin, INPUT_PULLUP); //declareren van de buttonpin als input
+  lcd.clear();
+  lcd.print("Hello World");
+  //pinMode(lcdRS,OUTPUT);
+  //pinMode(lcdRW,OUTPUT);
+  //pinMode(lcdEN,OUTPUT);
+  //pinMode(lcdd4,OUTPUT);
+  //pinMode(lcdd5,OUTPUT);
+  //pinMode(lcdd6,OUTPUT);
+  //pinMode(lcdd7,OUTPUT);
+  
   Serial.begin(9600);
+  
+  //Sendval(0,0,1,0,0,1,0);
+  //Sendval(0,0,1,1,0,0,0);
+  
+  
 }
 
 void loop()
@@ -27,4 +56,8 @@ void loop()
   
   }
 }
+
+
+
+
 
